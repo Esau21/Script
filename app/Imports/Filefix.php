7cfcbf45c28d442cf6330d98cc1ec33df8a3830d
Esau21 <esaumoran1999@gmail.com>
2024-06-05 11:55:20 -0600

    first commit

--- app/Imports/Filefix.php
+++ app/Imports/Filefix.php
@@ -18,34 +18,49 @@ class Filefix implements ToCollection, WithHeadingRow
     {
         DB::beginTransaction();
 
+        dd($collection);
         foreach ($collection as $item) {
-            /* dd($item['numero_documento']); */
-            $document = DB::selectOne("SELECT  * from documentos where concat (documento_serial,documento_code)=?", [
+            $document = DB::selectOne("SELECT * FROM documentos WHERE concat(documento_serial, documento_code) = ?", [
                 $item['numero_documento']
             ]);
 
-            if(is_null($document)){
-                dd('error el documento' . $item['numero_documento'] . 'No exite en la base de datos');
+            if (is_null($document)) {
+                dd('Error: el documento ' . $item['numero_documento'] . ' no existe en la base de datos');
             }
-            $document = Documento::findOrfail($document->id);
-            $document->fecha_emision = self::convertDateFormat($item['fecha']);
-            $document->save();
+
+            $document = Documento::findOrFail($document->id);
+
             
+            logger()->info('Fecha original: ' . $item['fecha']);
+
+            $formattedDate = self::convertDateFormat($item['fecha']);
+
+            
+            logger()->info('Fecha formateada: ' . $formattedDate);
+
+            $document->fecha_emision = $formattedDate;
+            $document->save();
         }
 
         DB::commit();
-
     }
 
+    
     public static function convertDateFormat($date)
     {
-        // Crear una instancia de Carbon desde la fecha proporcionada
-        $carbonDate = Carbon::createFromFormat('m/d/Y', trim($date));
+        try {
+            // Crear una instancia de Carbon desde la fecha proporcionada en el formato 'd/m/Y'
+            $carbonDate = Carbon::createFromFormat('d/m/Y', trim($date));
 
-        // Convertir la fecha al formato deseado
-        $formattedDate = $carbonDate->format('Y-m-d');
+            // Convertir la fecha al formato MySQL 'Y-m-d'
+            $formattedDate = $carbonDate->format('Y-m-d');
 
-        return $formattedDate;
+            return $formattedDate;
+        } catch (\Exception $e) {
+            // En caso de error, registrar el error y devolver null
+            logger()->error('Error al convertir la fecha: ' . $date . ' - ' . $e->getMessage());
+            return null;
+        }
     }
 
     public function mapping(): array
